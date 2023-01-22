ea932d8f5fbdb227d067381db3fe681c772516ad
Sergei Savich <s.savich@ipoteka.center>
2023-01-22 22:12:28 +0300

    added owner

--- src/test/java/com/vesska/TestBase.java
+++ src/test/java/com/vesska/TestBase.java
@@ -1,30 +1,19 @@
 package com.vesska;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import com.vesska.config.WebDriverProvider;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.remote.DesiredCapabilities;
+
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "100");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.baseUrl = System.getProperty("base_url", "https://demoqa.com");
-        Configuration.timeout = 10000;
-        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
+        WebDriverProvider.config();
     }
 
     @BeforeEach
---
+++ src/test/java/com/vesska/config/WebDriverConfig.java
@@ -0,0 +1,27 @@
+package com.vesska.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "classpath:${env}.properties"
+})
+
+public interface WebDriverConfig extends Config {
+
+    @Key("browserName")
+    @DefaultValue("chrome")
+    String getBrowserName();
+
+    @Key("browserVersion")
+    String getBrowserVersion();
+
+    @Key("browserSize")
+    String getBrowserSize();
+
+    @Key("baseUrl")
+    @DefaultValue("https://demoqa.com")
+    String getBaseUrl();
+
+    @Key("remoteUrl")
+    String getRemoteUrl();
+}
